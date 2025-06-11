9cd41599bf1f49ba9bb23d60ac89427a62c2a0e7
longnhatOI <tolongnhat1999@gmail.com>
2025-06-11 08:40:59 +0700

    fix lỗi

--- ss7_bien_va_kieu_du_lieu/chuyen_doi_tien_te.js
+++ ss7_bien_va_kieu_du_lieu/chuyen_doi_tien_te.js
@@ -1,19 +1,22 @@
 function ChangMoney() {
-    let Amount = document.getElementById("Amount").value;
+    let amount = document.getElementById("Amount").value;
     let From = document.getElementById("From").value;
     let To = document.getElementById("To").value;
     let Result;
-    if (From == "USD" && To == "VND") {
-        Result = "Result: " + (Amount * 26050) + " VND";
-    } else if (From == "VND" && To == "USD") {
-        Result = "Result : " + (Amount / 26050) + " USD";
-    } else if (From == "VND" && To == "VND") {
-        Result = "Result : " + Amount + " VND";
-    } else if (From == "USD" && To == "USD") {
-        Result = "Result : " + Amount + " USD";
-    } else {
-        Result = "Error";
-    }
-    document.getElementById("Result").innerHTML = Result;
+    if (!isNaN(amount) && amount > 0) {
 
+        console.log(amount, From, To)
+        if (From === "USD" && To === "VND") {
+            Result = "Result: " + (amount * 26050) + " VND";
+        } else if (From === "VND" && To === "USD") {
+            Result = "Result : " + (amount / 26050) + " USD";
+        } else {
+            Result = "Error";
+        }
+        document.getElementById("Result").innerHTML = Result;
+
+    }
+    else{
+        alert(`ban phai nhap so hop le`)
+    }
 }
