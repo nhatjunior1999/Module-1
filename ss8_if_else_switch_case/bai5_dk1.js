f4876c455d19d5dcf373177703d1715558e2c84c
longnhatOI <tolongnhat1999@gmail.com>
2025-06-12 11:35:38 +0700

    bai5

---
+++ ss8_if_else_switch_case/bai5_dk1.js
@@ -0,0 +1,17 @@
+let diemKiemTra = +prompt("Nhap diem kiem tra từ 1 - 10 điểm");
+let diemGiuaKi = +prompt("Nhap diem giua ki từ 1 đến 10 điểm");
+let diemCuoiKi = +prompt("Nhap diem cuoi ki từ 1 đến 10 điểm");
+let tongDiem = (diemKiemTra + diemGiuaKi + diemCuoiKi) / 3;
+
+if ( tongDiem >= 0 && tongDiem <= 10 ) {
+    if (tongDiem >= 9) {
+        alert("bạn được học sinh xuất sắc" + tongDiem.toFixed(2))
+    }else if(tongDiem >= 6.5 && tongDiem <= 8){
+        alert("học sinh trung bình khá" +" "+ tongDiem.toFixed(2))
+    }else if(tongDiem >=0 && tongDiem <= 6.5){
+        alert("học sinh kém " + tongDiem.toFixed(2))
+    }
+}
+else {
+    alert(" điểm của bạn đã quá mời nhập lại")
+}
---
+++ ss8_if_else_switch_case/bai_4_dk1.js
@@ -0,0 +1,12 @@
+let a = +prompt(" Nhập vào số thứ a ");
+let b = +prompt(" Nhập vào số thứ b ");
+let c = +prompt(" Nhập vào số thứ c ");
+if (a >= b && a >= c) {
+    alert(` ${a}  là A so lớn nhất `)
+} else if (b >= a && b >= c){
+    alert(` ${b}  là B so lớn nhất `)
+} else {
+    alert(` ${c}  là C so lớn nhất `)
+}
+
+
