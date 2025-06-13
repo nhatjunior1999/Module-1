948a7c1e8c1a449a396ecc4b85ea6b194fe79a76
longnhatOI <tolongnhat1999@gmail.com>
2025-06-13 19:33:33 +0700

    fixloi

--- ss8_if_else_switch_case/bai5_dk1.js
+++ ss8_if_else_switch_case/bai5_dk1.js
@@ -3,15 +3,16 @@ let diemGiuaKi = +prompt("Nhap diem giua ki từ 1 đến 10 điểm");
 let diemCuoiKi = +prompt("Nhap diem cuoi ki từ 1 đến 10 điểm");
 let tongDiem = (diemKiemTra + diemGiuaKi + diemCuoiKi) / 3;
 
-if ( tongDiem >= 0 && tongDiem <= 10 ) {
+if (tongDiem >= 0 && tongDiem <= 10) {
     if (tongDiem >= 9) {
         alert("bạn được học sinh xuất sắc" + tongDiem.toFixed(2))
-    }else if(tongDiem >= 6.5 && tongDiem <= 8){
-        alert("học sinh trung bình khá" +" "+ tongDiem.toFixed(2))
-    }else if(tongDiem >=0 && tongDiem <= 6.5){
+    } else if (tongDiem > 8) {
+        alert(" bạn được học sinh giỏi" + tongDiem.toFixed(2))
+    } else if (tongDiem >= 6.5) {
+        alert("học sinh trung bình khá" + " " + tongDiem.toFixed(5))
+    } else {
         alert("học sinh kém " + tongDiem.toFixed(2))
     }
-}
-else {
+} else {
     alert(" điểm của bạn đã quá mời nhập lại")
 }
