b0e4b4208229134312b5482837aedc1dfbc9c8da
longnhatOI <tolongnhat1999@gmail.com>
2025-06-17 09:09:57 +0700

    fix

--- ss10_For_do_while/bai_2_vl1.js
+++ ss10_For_do_while/bai_2_vl1.js
@@ -1,6 +1,5 @@
 let khaiBao;
-
-for (;;){
+ while(true){
      khaiBao = +prompt(" nhâp vào số nhiệt độ")
     if (100 <= khaiBao){
         alert(" nhiệt độ quá cao mời bạn nhập lại")
@@ -8,7 +7,7 @@ for (;;){
         alert(" nhiệt độ thấp mời bạn nhập lại")
     }else {
         alert(` nhiệt độ của bạn đạt chuẩn rồi ${khaiBao}`)
-        break
+        break;
     }
 
 }
--- ss10_For_do_while/hien_thi_cac_so_nguyen_to.js
+++ ss10_For_do_while/hien_thi_cac_so_nguyen_to.js
@@ -1,8 +1,9 @@
 let displayPrimeNumber = 20;
 let primeCount = 0;
 let number = 2;
+let count =0;
+
 while (primeCount<displayPrimeNumber){
-    let count =0;
     for (let i = 2; i <=number ; i++) {
         if (number%i===0){
             count++;
