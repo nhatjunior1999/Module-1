694cde487ac5c6313721194869d63f4e907c0536
tolongnhat <tolongnhat1999@gmai.com>
2025-06-18 23:07:09 +0700

    bai5

---
+++ ss12_mang_1/mang2/bai_5_m2.js
@@ -0,0 +1,8 @@
+let chuoi1 = prompt("Nhập chuỗi thứ nhất:");
+let chuoi2 = prompt("Nhập chuỗi thứ hai:");
+
+if (chuoi1 === chuoi2) {
+    document.write(" Hai chuỗi giống nhau.");
+} else {
+    document.write(" Hai chuỗi khác nhau.");
+}
