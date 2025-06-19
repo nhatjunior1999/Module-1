28031d114e6875614fae5e697d756e4782d43dae
longnhatOI <tolongnhat1999@gmail.com>
2025-06-19 19:00:55 +0700

    first

---
+++ ss13_ham/test2.js
@@ -0,0 +1,15 @@
+export function nhan(a) {
+    return a * a;
+}
+
+export function nhanAb(a, b) {
+    return a * b
+}
+
+export function tong(a) {
+    return a * a
+}
+export function tongAb(a,b){
+    return a + b
+}
+
---
+++ ss13_ham/test_1.js
@@ -0,0 +1,18 @@
+// function my(){
+//     let a = arguments[0]
+//     let b = arguments[6]
+//     alert("firstArg is :" +a)
+//     alert("first phan tu so  is :" +b)
+// }
+// my("nhat" , 1 , 3 ,5 , 6 , 6 ,"nga")
+// //----------------
+function number(){
+    let a = 10
+    let b = 15
+    let tong = a + b
+    return tong;
+}
+let a = 20 ;
+let sum = number()
+console.log(sum)
+console.log(a)
