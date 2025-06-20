59c3ddbcf86efd60cc35a96f33c33a334b55b662
longnhatOI <tolongnhat1999@gmail.com>
2025-06-20 08:46:32 +0700

    bai_tap_snt

---
+++ ss13_ham/kiem_tra_snt.js
@@ -0,0 +1,20 @@
+let number = +prompt("moi ban nhap vao 1 so")
+
+function primeNum() {
+    for (let i = 2; i < Math.sqrt(number); i++) {
+        if (number % i === 0) {
+            alert(`so ${number} không phải là số nguyên tố`)
+            return;
+
+        }
+
+    }
+    alert(`${number} là số nguyên to`)
+}
+
+
+if (!isNaN(number) && Number.isInteger(number) && number > 1){
+    primeNum(number)
+}else {
+    alert(`${number} vui lòng nhập số lớn hơn 1 `)
+}
